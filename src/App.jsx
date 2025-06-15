a2a1a44d1f4cdd414c30d38f0dbb391d64c216fb
thenoobchad <noobchaddy@gmail.com>
2025-06-15 13:33:57 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import {motion} from "framer-motion"
-import { FiArrowRight } from "react-icons/fi"
+import { FiArrowRight, FiHeart, FiMail, FiMapPin } from "react-icons/fi"
 import { SiGithub, SiTiktok, SiYoutube, } from "react-icons/si"
-import { TiSocialTwitter } from "react-icons/ti"
+import { TiHeart, TiSocialTwitter } from "react-icons/ti"
 
 import {twMerge} from "tailwind-merge"
 
 export default function App() {
-  return (
+  return (<>
     <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
-      <div className="mx-auto max-w-4xl grid grid-cols-12 gap-4 grid-flow-dense">
+      <motion.div
+        
+        initial="initial"
+        animate="animate"
+        transition={{
+          staggerChildren: 0.05,
+        }}
+        className="mx-auto max-w-4xl grid grid-cols-12 gap-4 grid-flow-dense">
         <HeaderBlock />
         <SocialBlock/>
-        <Block />
-        <Block />
-        <Block />
-        <Block />
-      </div>
-      </div>
+        <AboutBlock />
+        <LocationBlock />
+        <EmailListBlock />
+     
+      </motion.div>
+      <Footer />
+      </div></>
   )
 }
 
@@ -24,7 +32,27 @@ export default function App() {
 
 const Block = ({ className, ...rest }) => {
   return <motion.div
-   
+    
+    variants={{
+      initial:{
+        scale: 0.5,
+        y: 50,
+        opacity: 0,
+      },
+      animate: {
+        scale: 1,
+        y: 0,
+        opacity: 1,
+      }
+    }}
+    
+    transition={{
+      type: "spring",
+      mass: 3,
+      stiffness: 400,
+      damping:50,
+    }}
+
     className={twMerge("col-span-4 rounded-sm border border-zinc-700 bg-zinc-800 p-6", className)}
     {...rest}
   />
@@ -42,25 +70,91 @@ const HeaderBlock = () => {
 
 const SocialBlock = () => {
   return <>
-    <Block className="col-span-6 bg-red-500 md:col-span-3">
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-red-500 md:col-span-3">
       <a href="#" className="grid h-full place-content-center text-3xl text-white">
         <SiYoutube />
       </a>
     </Block>
-    <Block className="col-span-6 bg-black md:col-span-3">
+    <Block 
+      whileHover={{
+      rotate: "2.5deg",
+      scale: 1.1,
+      }}
+      className="col-span-6 bg-black md:col-span-3">
       <a href="#" className="grid h-full place-content-center text-3xl text-white">
         <SiGithub />
       </a>
     </Block>
-    <Block className="col-span-6 bg-zinc-50 md:col-span-3">
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-zinc-50 md:col-span-3">
       <a href="#" className="grid h-full place-content-center text-3xl text-black">
         <SiTiktok />
       </a>
     </Block>
-    <Block className="col-span-6 bg-blue-500 md:col-span-3">
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-blue-500 md:col-span-3">
       <a href="#" className="grid h-full place-content-center text-3xl text-white">
         <TiSocialTwitter />
       </a>
 </Block>
 </>
+}
+
+const AboutBlock = () => (
+  <Block className="col-span-12 text-3xl leading-snug">
+    <p className="text-zinc-400">My passion is building cool stuff. {" "}
+      <span>I build primarily with React, Tailwind CSS, and Framer Motion. I love this Stack so much that I have built over 22 websites with it.</span>
+    </p>
+  </Block>
+)
+
+const LocationBlock = () => (
+  <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
+    <FiMapPin className="text-3xl" />
+    <p className="text-center text-lg text-zinc-400">Cyberspace</p>
+  </Block>
+)
+
+const EmailListBlock = () => (
+  <Block className="col-span-12 md:col-span-9">
+  
+    <p className="mb-3 text-lg">Join my mailing list</p>
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="flex items-center gap-2">
+      <input type="email"
+        placeholder="Enter your email"
+        className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
+      />
+       <button type="submit"
+        placeholder="Enter your email"
+        className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
+      >
+        <FiMail/> Join the list
+      </button>
+    </form>
+  </Block>
+)
+
+const Footer = () => {
+  return (
+    <footer className="mt-12 w-full flex justify-center">
+      <p className="text-center text-zinc-400 flex">
+        Made with {"   "} <span><TiHeart color="red" /></span> {" "} by {" "}<a href="#" className="text-red-300">@chuddaie</a>
+      </p>
+    </footer>
+  )
 }
