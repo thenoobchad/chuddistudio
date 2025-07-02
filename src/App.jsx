5c18008e313b5e11dd419abaf861d22f7494071c
thenoobchad <noobchaddy@gmail.com>
2025-07-02 23:00:18 +0100

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,158 +3,52 @@ import { FiArrowRight, FiHeart, FiMail, FiMapPin } from "react-icons/fi"
 import { SiGithub, SiTiktok, SiYoutube, } from "react-icons/si"
 import { TiHeart, TiSocialTwitter } from "react-icons/ti"
 
-import {twMerge} from "tailwind-merge"
+
+
 
 export default function App() {
-  return (<>
-    <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
-      <motion.div
-        
-        initial="initial"
-        animate="animate"
-        transition={{
-          staggerChildren: 0.05,
-        }}
-        className="mx-auto max-w-4xl grid grid-cols-12 gap-4 grid-flow-dense">
-        <HeaderBlock />
-        <SocialBlock/>
-        <AboutBlock />
-        <LocationBlock />
-        <EmailListBlock />
-     
-      </motion.div>
-      <Footer />
-      </div></>
-  )
-}
-
-
-
-const Block = ({ className, ...rest }) => {
-  return <motion.div
-    
-    variants={{
-      initial:{
-        scale: 0.5,
-        y: 50,
-        opacity: 0,
-      },
-      animate: {
-        scale: 1,
-        y: 0,
-        opacity: 1,
-      }
-    }}
-    
-    transition={{
-      type: "spring",
-      mass: 3,
-      stiffness: 400,
-      damping:50,
-    }}
-
-    className={twMerge("col-span-4 rounded-sm border border-zinc-700 bg-zinc-800 p-6", className)}
-    {...rest}
-  />
-}
-
-const HeaderBlock = () => {
-  return <Block className="col-span-12 row-span-2 md:col-span-6">
-    <img src="https://pbs.twimg.com/profile_images/1932146465129844736/TAEJBusb_400x400.jpg" className="mb-4 size-18 rounded-full justify-self-center-safe" />
-    <h1 className="mb-12 text-4xl font-medium leading-tight ">Hi, I'm Henry.{" "} <span className="text-zinc-400">I build cool websites.</span></h1>
-    <a href="#" className="flex items-center gap-1  text-red-300 hover:underline ">
-      Contact me <FiArrowRight />
-    </a>
-  </Block>
-}
-
-const SocialBlock = () => {
-  return <>
-    <Block
-      whileHover={{
-        rotate: "2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-red-500 md:col-span-3">
-      <a href="#" className="grid h-full place-content-center text-3xl text-white">
-        <SiYoutube />
-      </a>
-    </Block>
-    <Block 
-      whileHover={{
-      rotate: "2.5deg",
-      scale: 1.1,
-      }}
-      className="col-span-6 bg-black md:col-span-3">
-      <a href="#" className="grid h-full place-content-center text-3xl text-white">
-        <SiGithub />
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "-2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-zinc-50 md:col-span-3">
-      <a href="#" className="grid h-full place-content-center text-3xl text-black">
-        <SiTiktok />
-      </a>
-    </Block>
-    <Block
-      whileHover={{
-        rotate: "-2.5deg",
-        scale: 1.1,
-      }}
-      className="col-span-6 bg-blue-500 md:col-span-3">
-      <a href="#" className="grid h-full place-content-center text-3xl text-white">
-        <TiSocialTwitter />
-      </a>
-</Block>
-</>
-}
-
-const AboutBlock = () => (
-  <Block className="col-span-12 text-3xl leading-snug">
-    <p className="text-zinc-400">My passion is building cool stuff. {" "}
-      <span>I build primarily with React, Tailwind CSS, and Framer Motion. I love this Stack so much that I have built over 22 websites with it.</span>
-    </p>
-  </Block>
-)
-
-const LocationBlock = () => (
-  <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
-    <FiMapPin className="text-3xl" />
-    <p className="text-center text-lg text-zinc-400">Cyberspace</p>
-  </Block>
-)
-
-const EmailListBlock = () => (
-  <Block className="col-span-12 md:col-span-9">
-  
-    <p className="mb-3 text-lg">Join my mailing list</p>
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="flex items-center gap-2">
-      <input type="email"
-        placeholder="Enter your email"
-        className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
-      />
-       <button type="submit"
-        placeholder="Enter your email"
-        className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
-      >
-        <FiMail/> Join the list
-      </button>
-    </form>
-  </Block>
-)
-
-const Footer = () => {
   return (
-    <footer className="mt-12 w-full flex justify-center">
-      <p className="text-center text-zinc-400 flex">
-        Made with {"   "} <span><TiHeart color="red" /></span> {" "} by {" "}<a href="#" className="text-red-300">@chuddaie</a>
-      </p>
-    </footer>
+    <div>
+      <header className="h-18 border-b-4 flex items-center justify-between shadow-">
+
+        <nav className="w-full flex justify-between pr-4 gap-4">
+          <ul className="flex items-center gap-4 text-gray-800 font-semibold pl-8">
+          <li>About us</li>
+            <li>Services</li>
+            <li>Case studies</li>
+            </ul>
+        </nav>
+
+        <a href="/" className="h-full flex items-center justify-center border-l-2 px-8 text-xl font-bold tracking-tighter ">chudd <span className="text-orange-600">i</span> studio</a>
+
+        {/* <nav className="w-full flex justify-end pr-4 gap-4">
+          <Button><SiGithub size={25} /></Button>
+          
+          <Button><TiSocialTwitter size={25} /></Button>
+          
+          <Button><SiTiktok  size={25}/></Button>
+        </nav> */}
+      </header>
+    <div className="p-2 flex flex-col min-h-screen items-center justify-center">
+        <div className="max-w-152 md:max-w-202 gap-4 flex flex-col">
+          <span className="bg-gray-300 w-fit py-1 px-3 rounded-full uppercase text-xs tracking-widest font-semibold">design meets strategy</span>
+          <h4 className="text-4xl md:text-7xl font-extrabold">Empowering your brand in the digital age. </h4>
+          <p className="text-md text-gray-600">We are a creative studio helping innovative startups and business stand out in a world of short attention sapn with strategic web designs and branding</p>
+
+          <Button className="text-base font-semibold w-fit bg-orange-400">Let's chat!</Button>
+</div>
+
+   
+      </div>
+      </div>
   )
+}
+
+
+
+const Button = ({ children, className }) => {
+  return(
+    <button type="submit" className={`h-12 shadow-[4px_4px_0px_0px] cursor-pointer border-2 px-4 text-md tracking-wide flex items-center  rounded-sm hover:shadow-[0px_0px_0] transition-all ${className}`}>
+  {children}
+</button>)
 }
