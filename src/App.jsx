aef30e214da08bdd92fadae2374c285c86a97caa
thenoobchad <noobchaddy@gmail.com>
2025-07-03 20:30:53 +0100

    demo cards

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,18 @@ export default function App() {
 
         <nav className="w-full flex justify-between pr-4 gap-4">
           <ul className="flex items-center gap-4 text-gray-800 font-semibold pl-4">
-          <li className="whitespace-nowrap tracking-tight">About us</li>
-            <li className="whitespace-nowrap tracking-tight">Services</li>
-            <li className="whitespace-nowrap tracking-tight">Case studies</li>
+          <a href="#" className="whitespace-nowrap tracking-tight">About us</a>
+            <a href="#" className="whitespace-nowrap tracking-tight">Services</a>
+            <a href="#" className="whitespace-nowrap tracking-tight">Case studies</a>
             </ul>
         </nav>
 
         <a href="/" className="h-full flex items-center justify-center border-l-2 px-6 text-xl font-bold tracking-tighter ">chudd <span className="text-orange-600">i</span> studio</a>
 
       </header>
+      {/* HEADER ENDS HERE */}
+      
+      {/* HERO STARTS HERE */}
     <div className="p-2 flex flex-col min-h-screen items-center justify-center">
         <div className="max-w-152 md:max-w-202 gap-4 flex flex-col">
           <span className="bg-gray-300 w-fit py-1 px-3 rounded-full uppercase text-xs tracking-widest font-semibold">design meets strategy</span>
@@ -30,22 +33,53 @@ export default function App() {
 
           <Button className="text-base font-semibold w-fit bg-orange-400">Let's chat!</Button>
 
-          <p></p>
-
-
-
          
       </div>
       </div>
+
+
+      {/* HERO ENDS HERE */}
+
+      {/* ABOUT US BEGINS HERE */}
+      <section>
+
+      </section>
+      {/* ABOUT US ENDS HERE */}
+
+      
+      {/* SERVICES BEGINS HERE */}
+      <section className="bg-white px-2 py-6">
+
+        
+        {/* PROJECTS BEGINS  */}
+        <div className="w-full flex flex-col items-center gap-2">
+          <h4 className="text-xl font-extrabold uppercase">Projects</h4>
+          <div className="h-1 w-15 bg-black" />
+          <p className="max-w-152 md:max-w-202">Some business and startup websites we ahve created over the years with each project containing it's own case study. each tailored to meet our clients needs. Kindly explore our catalog.</p>
+        </div>
+
+        <div className="py-6">
+
+          <div className="border-2 max-w-[400px] sm:w-[300px] h-[350px] shadow-[4px_4px_0px_0px] rounded-sm p-2">
+            <span>View site</span>
+          </div>
+        </div>
+
+         {/* PROJECTS ENDS  */}
+      </section>
+      {/* SERVICES ENDS HERE */}
+
       <footer className="w-full bg-black px-2 pt-10 text-gray-50 flex flex-col">
         <div className="flex flex-col md:flex-row w-full justify-between">
           <div className="flex flex-col gap-4 w-full md:max-w-md">
-            <h4 className="uppercase font-extrabold text-xl tracking-tight">Chidi Elueme</h4>
+            <h4 className="uppercase font-bold text-[18px] tracking-tight">Chidi Elueme</h4>
             <p className="text-[14px]">A Fullstack focused web developer with expertise in building websites and web applications that drive the need market and enhances business and products.</p>
         </div>
           <div className="flex gap-4 justify-center py-4">
-           
+           <a href="https://github.com/thenoobchad">
               <SiGithub className="w-5 h-6" />
+            </a>
+            
             
             <TiSocialTwitter className="w-6 h-6" />
             <TiSocialDribbble className="w-6 h-6" />
