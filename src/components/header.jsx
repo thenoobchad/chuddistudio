681cfd5a5557b148644e2fdaf883b39ef6ff8cf4
thenoobchad <noobchaddy@gmail.com>
2025-09-01 22:41:57 +0100

    header fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,28 +21,26 @@ export const Header = () => {
     
     window.addEventListener("click", setIsOpen(false))
   },[])
-
- 
-  
  
 
   return (
     <header className={`${active ? "fixed w-full   top-0  p-2 transition-all delay-200 !h-18":""} h-14 flex items-center justify-between z-30`}>
 
-      <nav className={`${active && "bg-zinc-950"} w-full flex justify-between  gap-4 relative h-full items-center px-4`}>
+      {!isOpen && <nav className={`${active && "bg-zinc-950"} w-full flex justify-between  gap-4 relative h-full items-center px-4`}>
         <a>HEO</a>
-      <ul className="flex items-center gap-4 text-gray-100 font-semibold pl-4 z-10">
-      <a href="#" className="whitespace-nowrap tracking-tight">About us</a>
-        <a href="#" className="whitespace-nowrap tracking-tight">Services</a>
-        <a href="#" className="whitespace-nowrap tracking-tight">Case studies</a>
+        <ul className="flex items-center gap-4 text-gray-100 font-semibold pl-4 z-10">
+          <a href="#" className="whitespace-nowrap tracking-tight">About us</a>
+          <a href="#" className="whitespace-nowrap tracking-tight">Services</a>
+          <a href="#" className="whitespace-nowrap tracking-tight">Case studies</a>
         </ul>
 
         <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
-    </nav>
+      </nav>}
 
     
-      <div className={`${!isOpen ? "translate-x-[100%] transition-all duration-300" : "translate-x-[0%] transition-all duration-300"} absolute top-0 right-0 z-50 w-[50%] md:w-[30%] xl:w-[20%] min-h-screen bg-gray-950 `}>
+      <div className={`${!isOpen ? "translate-x-[100%] transition-all duration-300" : "translate-x-[0%] transition-all duration-300"} absolute top-0 right-0 z-50 w-[50%] md:w-[30%] xl:w-[20%] min-h-screen bg-zinc-950`}>
         <span className="h-14 flex items-center">navigation</span>
+        <span onClick={() => setIsOpen(false)}>Exit</span>
         <div className="w-full h-full flex flex-col items-center">
           <ul className="flex flex-col py-4 gap-4">
             <li>About</li>
