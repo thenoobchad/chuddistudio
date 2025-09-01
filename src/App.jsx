ebe29efb5f5d1cac48567ff2dc65f33794aa7913
thenoobchad <noobchaddy@gmail.com>
2025-09-01 18:37:00 +0100

    images to bg

--- src/App.jsx
+++ src/App.jsx
@@ -63,16 +63,16 @@ export default function App() {
     {/* HERO STARTS HERE */}
 
   
-    <section id="hero" className=" p-2 py-6 flex flex-col h-screen items-center justify-center">
-        <div className=" max-w-152 md:max-w-202 gap-4 flex flex-col">
+      <section id="hero" className=" p-2 py-6 flex flex-col h-screen relative items-center justify-center bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url('/files/image-2.png')" }}>
+        <div className="absolute inset-0 bg-black/90 z-10"/>
+        <div className="relative z-20 max-w-152 md:max-w-202 gap-4 flex flex-col">
 
           
           
           <h4 className="text-4xl md:text-6xl font-extrabold ">Hi, i'm Henry <br />  Elueme Ositadimma</h4>
           
         <h3 className="font-semibold">Full Stack Web Developer & Software Engineer . . . </h3>
-        {/* <p className="text-md bg-purple-100 text-gray-900 border-r-4 font-semibold text-sm pr-4 p-2 italic">I am a dedicated full stack developer with over 4 years of experience, passionately crafting innovative, scalable, and user-centric web applications.</p> */}
-
+        
           <div className="flex gap-4">
 
             <a href="/files/henry_cv.pdf" download>
@@ -89,15 +89,23 @@ export default function App() {
 
     {/* HERO ENDS HERE */}
 
-    {/* ABOUT US BEGINS HERE */}
+    {/* ABOUT ME */}
     <section id="about" className="text-white bg-purple-950 p-2 py-6 flex flex-col min-h-screen items-center justify-center">
         <div className="max-w-152 md:max-w-202  mx-auto w-full">
-          
-          <h4 className="text-md underline  font-bold my-8">about me</h4>
+           <h4 className="text-md underline  font-bold my-8">about me</h4>
           <h4 className="text-3xl capitalize font-semibold text-center pb-4">A web developer passionate about <br/>bringing ideas to life</h4>
-          <p>I am a Full stack developer specializing in frontend development with a strong understnding of backend architecture, I build dynamic and scalable applications. </p>
-          <p>I am a Full stack developer specializing in frontend development with a strong understnding of backend architecture, I build dynamic and scalable applications. </p>
-        
+          <div className="flex flex-col md:flex-row gap-4">
+             <div className="w-1/2">
+              
+              <img src="/files/hero.png" className="h-full w-full bg-cover bg-no-repeat"/>
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+         
+              <p className="text-justify">I am a Full stack developer specializing in frontend development with a strong understnding of backend architecture, I build dynamic and scalable applications. </p>
+            </div>
+           
+          
+        </div>
       </div>
         
     </section>
@@ -205,24 +213,24 @@ export default function App() {
             <form className="w-full ">
               <div className="flex flex-col gap-2 my-4">
                 <label htmlFor="full name">Full Name</label>
-                <input type="text" placeholder="e.g.John Doe" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50 px-4 border-b border-zinc-400"/>
+                <input type="text" placeholder="e.g.John Doe" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50/10 px-4 border-b border-zinc-400"/>
               </div>
 
               <div className="flex flex-col gap-2 my-4">
                 <label htmlFor="full name">Email</label>
-                <input type="text" placeholder="e.g.johndoe@mail.com" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50 px-4 border-b border-zinc-400"/>
+                <input type="text" placeholder="e.g.johndoe@mail.com" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50/10 px-4 border-b border-zinc-400"/>
               </div>
 
               <div className="flex flex-col gap-2 my-4">
                 <label htmlFor="full name">Subject</label>
-                <input type="text" placeholder="project collaboration" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50 px-4 border-b border-zinc-400"/>
+                <input type="text" placeholder="project collaboration" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50/10 px-4 border-b border-zinc-400"/>
               </div>
 
               <div className="flex flex-col gap-2 my-4">
                 <label htmlFor="full name">Message</label>
-                <textarea cols={1} rows={3} type="text" placeholder="tell me about your project" className="hover:outline-0 outline-0 h-10 text-sm bg-zinc-50 px-4 border-b border-zinc-400"/>
+                <textarea cols={1} rows={3} type="text" placeholder="tell me about your project" className="hover:outline-0 outline-0 h-full text-sm bg-zinc-50/10 py-2 px-4 border-b border-zinc-400"/>
               </div>
-               <Button className="mt-4 text-base font-semibold w-full text-white text-center bg-purple-950">Send Message</Button>
+               <Button className="mt-7 text-base font-semibold w-full text-white text-center bg-purple-950 justify-center">Send Message</Button>
             </form>
           </div>
     </div>
@@ -239,7 +247,7 @@ export default function App() {
 
 const Button = ({ children, className }) => {
   return(
-    <button type="submit" className={`h-12 shadow-[4px_4px_0px_0px] shadow-black cursor-pointer  px-4 text-md tracking-wide flex items-center  rounded-sm hover:shadow-[0px_0px_0] transition-all ${className}`}>
+    <button type="submit" className={`h-12 shadow-[4px_4px_0px_0px] shadow-black cursor-pointer  px-4 text-md tracking-wide flex items-center hover:shadow-[0px_0px_0] transition-all ${className}`}>
   {children}
 </button>)
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react"
 
 
 export const Header = () => {
-  return (
-    <header className="h-10 flex items-center justify-between bg-white">
+  const [active, setActive] = useState(false)
 
-    <nav className="w-full flex justify-between pr-4 gap-4">
-      <ul className="flex items-center gap-4 text-gray-800 font-semibold pl-4">
+  const scrollYposition = () => {
+    window.scrollY > 40 ? setActive(true) : setActive(false);
+    console.log(active)
+  }
+
+  useEffect(() => {
+    
+    window.addEventListener("scroll", scrollYposition)
+
+    return () => window.removeEventListener("scroll", scrollYposition)
+  })
+
+  return (
+    <header className={`${active ? "fixed w-full   top-0  p-2 transition-all delay-200 !h-18":""} h-14 flex items-center justify-between `}>
+
+      <nav className={`${active && "bg-zinc-950"} w-full flex justify-between  gap-4 relative h-full`}>
+        <a>HEO</a>
+      <ul className="flex items-center gap-4 text-gray-100 font-semibold pl-4 z-10">
       <a href="#" className="whitespace-nowrap tracking-tight">About us</a>
         <a href="#" className="whitespace-nowrap tracking-tight">Services</a>
         <a href="#" className="whitespace-nowrap tracking-tight">Case studies</a>
         </ul>
+
+        menu
     </nav>
 
     
@@ -18,3 +36,6 @@ export const Header = () => {
   </header>
   )
 }
+
+
+
